4e4461f901678b8ce4c5c19f1b58d5a4f35c1f51
Denilson Fodor <Denilson.fodor@newtechs.com.br>
2022-12-22 09:08:25 -0300

    aula Tyupescript

--- src/exemplos/arrays.ts
+++ src/exemplos/arrays.ts
@@ -1,7 +1,7 @@
 const alunos = ['Denilson', 'Paulo', 'Antonio', 1234];
 
 export function listaDeAlunos() {
- /*
+  /*
     for (let contador = 0; contador < alunos.length; contador++) {
     const element = alunos[contador];
 
@@ -19,44 +19,44 @@ export function listaDeAlunos() {
         }
     );
     */
-   meuForEach(alunos, mostraAluno);
+  meuForEach(alunos, mostraAluno);
 }
 
 function mostraAluno(item: any) {
-    console.log(item);
+  console.log(item);
 }
 
 function meuForEach(colecao: Array<any>, callback: Function) {
-    for (const elemento of colecao) {
-        callback(elemento);
-    }    
+  for (const elemento of colecao) {
+    callback(elemento);
+  }
 }
 
 export function somaNotas(notas: Array<number>) {
-    /*
+  /*
     let soma = 0;
     for (const nota of notas) {
         soma = soma + nota;
     }
     return soma;
     */
-   return notas.reduce((soma, nota) => soma + nota , 0);
+  return notas.reduce((soma, nota) => soma + nota, 0);
 }
 
 class Aluno {
-   constructor(public nome: string, public nota: number) {}
+  constructor(public nome: string, public nota: number) {}
 }
 
 export function montaTurma() {
-    return [
-        new Aluno('Denilson', 10),
-        new Aluno('Sandra', 8),
-        new Aluno('Marcos', 8),
-    ];
+  return [
+    new Aluno('Denilson', 10),
+    new Aluno('Sandra', 8),
+    new Aluno('Marcos', 8),
+  ];
 }
-   
+
 export function filtraAlunoPorNota(turma: Array<Aluno>, nota: number) {
-/*
+  /*
     let turmaFiltrada: any = [];
 
     for (const aluno of turma) {
@@ -67,22 +67,20 @@ export function filtraAlunoPorNota(turma: Array<Aluno>, nota: number) {
 
     return turmaFiltrada;
     */
-   return turma.filter((aluno) => aluno.nota === nota);
+  return turma.filter((aluno) => aluno.nota === nota);
 }
 
 export function buscaAlunoPorNota(turma: Array<Aluno>, nota: number) {
-    return turma.find((alunos) => alunos.nota === nota);
+  return turma.find((alunos) => alunos.nota === nota);
 }
 
 export function tirarPontoTurma(turma: Array<Aluno>, pontoNegativo: number) {
-    let turmaNegativa: Array<Aluno> = [];
+  let turmaNegativa: Array<Aluno> = [];
 
-    turmaNegativa = turma.map((aluno) => {
-        aluno.nota -= pontoNegativo;
-        return aluno;
-    });
+  turmaNegativa = turma.map((aluno) => {
+    aluno.nota -= pontoNegativo;
+    return aluno;
+  });
 
-    return turmaNegativa;
+  return turmaNegativa;
 }
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { buscaAlunoPorNota, listaDeAlunos, montaTurma, somaNotas, tirarPontoTurma } from "./exemplos/arrays";
-import { exemploIfLoop, exemploTipos } from "./exemplos/primitivos";
-import { filtraAlunoPorNota } from "./exemplos/exemplo-objetos";
-
+import {
+  buscaAlunoPorNota,
+  listaDeAlunos,
+  montaTurma,
+  somaNotas,
+  tirarPontoTurma,
+} from './exemplos/arrays';
+import { exemploIfLoop, exemploTipos } from './exemplos/primitivos';
+import { filtraAlunoPorNota } from './exemplos/exemplo-objetos';
 
 const turma = montaTurma();
 
 console.log(filtraAlunoPorNota(turma));
-
-
-
